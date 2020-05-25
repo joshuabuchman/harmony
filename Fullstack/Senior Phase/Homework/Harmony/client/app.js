383fdf182cf165203910844069137e48e48fd91e
Joshua Buchman <joshua.buchman@msn.com>
2020-05-24 21:53:48 -0400

    message

---
+++ Fullstack/Senior Phase/Homework/Harmony/client/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import {Navbar} from './components'
+import Routes from './routes'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes />
+    </div>
+  )
+}
+
+export default App
